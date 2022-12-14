b1669a36805d4824c44722ad03a99ae746bc4fde
John Dzikunu <john.dzikunu@malitech.com>
2022-12-14 15:12:45 +0000

    back end initial codes

---
+++ Front-end/PayRoll/src/test/java/com/amalitech/payroll/payroll/PayRollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.payroll.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayRollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
