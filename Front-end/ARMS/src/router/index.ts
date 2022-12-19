bb407338b0604bedd71b7b56ae67276e9ad993ed
John Tekpor Dzikunu <dzikunujohn36@gmail.com>
2022-12-19 07:15:36 +0000

    ARMS static frontend pages added

--- Front-end/ARMS/src/router/index.ts
+++ Front-end/ARMS/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import HomeView from '../views/Dashboard.vue'
+import Management from '../views/Management.vue'
+import Employee from '../views/Employee.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,13 +11,23 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/management',
+      name: 'management',
+      component: Management
+    },
+    {
+      path: '/employee',
+      name: 'employee',
+      component: Employee
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
