1a2dd250949dd3c91eb35a39715860afb93461ba
John Tekpor Dzikunu <dzikunujohn36@gmail.com>
2022-12-28 17:54:12 +0000

    ARMS backend testing

--- Back-end/ARMS/app/Http/Controllers/EmployeeController.php
+++ Back-end/ARMS/app/Http/Controllers/EmployeeController.php
@@ -149,24 +149,27 @@ class EmployeeController extends Controller
      * @param string $id
      * @return Response
      */
-    public function update(Request $request, string $id): Response
+    public function update(Request $request, string $id)
     {
+
         $employee =  Employee::where("user_id", $id)->first(['*']);
 
-        if (($request->user()->role=="admin" || $request->user()->user_id == $id)
-            && $employee) {
+        if ($employee && ($request->user()->user_id == $id || $request->user()->role=="admin")) {
 
-            $this->checkAvailable($request);
-            $employee->save();
+            $this->checkAndValidate($request, $employee);
 
-            return new Response([
-                "message"=>"User information updated successfully",
-                "data"=>$employee
-            ]);
+            if ($request->get("hire_date")) {
+                $request->validate(['hire_date' => Constants::REQUIRE."|date"]);
+                $employee->hire_date = $request->get("hire_date");
+            }
+
         }
+
+         $employee->save();
         return new Response([
-            "message"=>"unauthorized request"
-        ], 401);
+            "message"=> "User information updated successfully",
+            "data"=>$employee
+        ]);
     }
 
     /**
@@ -224,36 +227,45 @@ class EmployeeController extends Controller
        $page = $page * $limit;
    }
 
- public  function checkAvailable(Request $request)
- {
-        global $employee;
-         if ($request->get("first_name")) $employee->first_name = $request->get("first_name");
-         if ($request->get("last_name")) $employee->last_name = $request->get("last_name");
-         if ($request->get("position")) $employee->position = $request->get("position");
-         if ($request->get("gender")) $employee->gender = $request->get("gender");
-         if ($request->get("department")) $employee->department = $request->get("department");
+   public function checkAndValidate(Request $request, Employee $employee)
+   {
+       if ($request->get("first_name")) {
+           $employee->first_name = $request->get("first_name");
+       }
 
-     if ($request->get("tell")) {
-         $request->validate(['tell'=>'min:9|max:10']);
-         if (is_numeric($request->get("tell")) != 1) {
-             return new Response([
-                 'message'=>["The tell must be numeric characters."],
-                 'errors'=>["The tell must be numeric characters."],
-             ], 422);
-         }
-         $employee->tell = $request->get("tell");
-     }
-     if ($request->get("other_names")) {
-         if (trim(strtolower($request->get("other_names"))) == "delete") {
-             $employee->other_names = null;
-         } else {
-             $employee->other_names = $request->get("other_names");
-         }
-     }
-     if ($request->get("hire_date")) {
-         $request->validate(['hire_date' => Constants::REQUIRE."|date"]);
-         $employee->hire_date = $request->get("hire_date");
-     }
- }
+       if ($request->get("last_name")) {
+           $employee->last_name = $request->get("last_name");
+       }
+
+
+
+       if ($request->get("tell")
+           && is_numeric($request->get("tell"))
+           && strlen($request->get("tell")) >=10) {
+
+           $employee->tell = $request->get("tell");
+
+       }
+
+       if ($request->get("position")) {
+           $employee->position = $request->get("position");
+       }
+
+       if ($request->get("other_names")) {
+           if (trim(strtolower($request->get("other_names"))) == "delete") {
+               $employee->other_names = null;
+           } else {
+               $employee->other_names = $request->get("other_names");
+           }
+       }
+
+       if ($request->get("gender")) {
+           $employee->gender = $request->get("gender");
+       }
+
+       if ($request->get("department")) {
+           $employee->department = $request->get("department");
+       }
+   }
 
 }
