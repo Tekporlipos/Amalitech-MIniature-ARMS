4e4d518e419ebb9cf5c794b7f9e1cb556003e510
John Tekpor Dzikunu <dzikunujohn36@gmail.com>
2023-01-02 19:00:22 +0000

    payroll testing added

--- Back-end/PayRoll/src/test/java/com/amalitech/payroll/PayRollApplicationTests.java
+++ Back-end/PayRoll/src/test/java/com/amalitech/payroll/PayRollApplicationTests.java
@@ -1,16 +1,12 @@
 package com.amalitech.payroll;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
 
 @SpringBootTest
-@ActiveProfiles("test")
 class PayRollApplicationTests {
-    @Test
-    void init(){
 
+    @Test
+    void contextLoads() {
     }
 }
